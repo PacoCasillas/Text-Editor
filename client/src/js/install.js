0aea0038660edb92c05bcb2776a3796e02a033db
PacoCasillas <pacocasillasmx@gmail.com>
2023-07-13 12:12:32 -0400

    add logic for database, add logic for inst PWA

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,34 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // prevent showing the default install prompt
+    event.preventDefault();
+    // Stash the event so it can be triggered later
+    window.deferredPrompt = event;
+    // remove the `hidden` attribute from the install button
+    butInstall.removeAttribute('hidden');
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    // retrieve the deferred prompt
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+    // show the install prompt
+    promptEvent.prompt();
+    // wait for the user to respond to the prompt
+    const result = await promptEvent.userChoice;
+    // hide the install button
+    butInstall.setAttribute('hidden', true);
+    // clear the deferred prompt
+    window.deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // log the installation
+    console.log('Jate was installed.', event);
+});
